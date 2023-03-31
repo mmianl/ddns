7ebe8983f38bdc8e96bd57ac653381e80a728e17
Markus Muellner <mmuellner@interact.nl>
2023-03-31 20:36:23 +0200

    setup

---
+++ cloudflare_dns.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+type CloudflareDNSProviderConfig struct {
+	// Swtich to enable or disable this provider
+	Enable bool `yaml:"enable"`
+
+	// Cloudflare API Token with "All zones - DNS:Read, DNS:Edit" permissions
+	APIToken string `yaml:"apiToken"`
+
+	// Cloudfare Zone ID
+	ZoneID string `yaml:"zoneID"`
+
+	// List of A Records
+	ARecords []string `yaml:"aRecords"`
+}
+
+type cloudfareListRecordsResponse struct {
+	Result []cloudfareListRecordsResponseResult `json:"result"`
+}
+
+type cloudfareListRecordsResponseResult struct {
+	ID         string                                 `json:"id"`
+	ZoneID     string                                 `json:"zone_id"`
+	ZoneName   string                                 `json:"zone_name"`
+	Name       string                                 `json:"name"`
+	Type       string                                 `json:"type"`
+	Content    string                                 `json:"content"`
+	Proxiable  bool                                   `json:"proxiable"`
+	Proxied    bool                                   `json:"proxied"`
+	TTL        int64                                  `json:"ttl"`
+	Locked     bool                                   `json:"locked"`
+	Meta       cloudfareListRecordsResponseResultMeta `json:"meta"`
+	Comment    string                                 `json:"comment"`
+	Tags       []string                               `json:"tags"`
+	CreatedOn  time.Time                              `json:"created_on"`
+	ModifiedOn time.Time                              `json:"modified_on"`
+}
+
+type cloudfareListRecordsResponseResultMeta struct {
+	AutoAdded           bool   `json:"auto_added"`
+	ManagedByApps       bool   `json:"managed_by_apps"`
+	ManagedByArgoTunnel bool   `json:"managed_by_argo_tunnel"`
+	Source              string `json:"source"`
+}
+
+type cloudfareUpdateRecordPayload struct {
+	Name    string `json:"name"`
+	Type    string `json:"type"`
+	Content string `json:"content"`
+}
+
+var defaultCloudflareDNSProviderConfig = &CloudflareDNSProviderConfig{
+	Enable:   false,
+	APIToken: "",
+	ZoneID:   "",
+	ARecords: nil,
+}
+
+type CloudflareDNSProvider struct {
+	apiToken string
+	zoneID   string
+	aRecords []string
+}
+
+// Returns an instance of CloudflareDNSProvider based on the passed configuration
+func NewCloudflareDNSProvider(config *CloudflareDNSProviderConfig) *CloudflareDNSProvider {
+	return &CloudflareDNSProvider{
+		apiToken: config.APIToken,
+		zoneID:   config.ZoneID,
+		aRecords: config.ARecords,
+	}
+}
+
+// Return the RecordAddressMappings with the current ip addresses for names specified in the configuration
+func (c *CloudflareDNSProvider) GetARecordAddresses() ([]RecordAddressMapping, error) {
+	// Make request
+	requestURL := fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records", c.zoneID)
+
+	req, err := http.NewRequest("GET", requestURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.apiToken))
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	// Parse body
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("response status code was %s, not 200", res.Status)
+	}
+
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyString := string(bodyBytes)
+
+	var r cloudfareListRecordsResponse
+	if err := json.Unmarshal([]byte(bodyString), &r); err != nil {
+		return nil, err
+	}
+
+	return GetContentsByNames(r, c.aRecords)
+}
+
+// Return the RecordAddressMappings with the current ip addresses for the provided names
+func GetContentsByNames(response cloudfareListRecordsResponse, names []string) ([]RecordAddressMapping, error) {
+	var ms []RecordAddressMapping
+
+	for _, name := range names {
+
+		m, err := GetContentByName(response, name)
+		if err != nil {
+			return nil, err
+		}
+
+		ms = append(ms, *m)
+	}
+
+	if len(ms) != len(names) {
+		return nil, errors.New("not all a records could not found")
+	}
+
+	return ms, nil
+}
+
+// Return the RecordAddressMapping with the current ip address for the provided name
+func GetContentByName(response cloudfareListRecordsResponse, name string) (*RecordAddressMapping, error) {
+	for _, item := range response.Result {
+		if item.Name == name {
+			return &RecordAddressMapping{id: item.ID, aRecord: name, ipAddress: item.Content}, nil
+		}
+	}
+
+	return nil, fmt.Errorf("no record with name %s was found", name)
+}
+
+// Set the provided A record to the provided ip address
+func (c *CloudflareDNSProvider) SetARecordAddress(ipAddress string, m RecordAddressMapping) error {
+	log.Info().Msgf("Setting A record %s to %s", m.aRecord, m.ipAddress)
+
+	// Make request
+	requestURL := fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records/%s", c.zoneID, m.id)
+	payload := &cloudfareUpdateRecordPayload{
+		Name:    m.aRecord,
+		Type:    "A",
+		Content: ipAddress,
+	}
+
+	json, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	log.Debug().Msgf("Executing put request against %s with payload %s", requestURL, json)
+	req, err := http.NewRequest("PUT", requestURL, bytes.NewBuffer(json))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.apiToken))
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	// Parse body
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("response status code from %s was %s, not 200", requestURL, res.Status)
+	}
+
+	return nil
+}
---
+++ ddns.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"runtime"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+type IPAddressProvider interface {
+	// Get the current ip address from provider
+	GetIPAddress() (*string, error)
+}
+
+type DNSProvider interface {
+	// Get the ip addresses which are currently set for the A records
+	GetARecordAddresses() ([]RecordAddressMapping, error)
+
+	// Set the current ip address for the provided A record
+	SetARecordAddress(string, RecordAddressMapping) error
+}
+
+type Retryable func() error
+
+type RecordAddressMapping struct {
+	id        string
+	aRecord   string
+	ipAddress string
+}
+
+var Version = "development"
+
+// Repeatedly run the Retryable function and wait between successful and failed attempts
+func Retry(retryable Retryable, waitInterval time.Duration, retryInterval time.Duration) {
+	for {
+		err := retryable()
+		if err == nil {
+			log.Info().Msgf("Success. Next attempt in %s", waitInterval)
+			time.Sleep(waitInterval)
+		} else {
+			log.Error().Msgf("An error occurred: %s. Retrying in %s", err, retryInterval)
+			time.Sleep(retryInterval)
+		}
+	}
+}
+
+// Updates the A records if required
+func SyncRecords(i IPAddressProvider, d DNSProvider) func() error {
+	return func() error {
+		addressToSet, err := i.GetIPAddress()
+		if err != nil {
+			return err
+		}
+		log.Info().Msgf("Obtained ip address was %s", *addressToSet)
+
+		setAddresses, err := d.GetARecordAddresses()
+		if err != nil {
+			return err
+		}
+
+		for _, addr := range setAddresses {
+			log.Info().Msgf("A record for %s is currently set to %s", addr.aRecord, addr.ipAddress)
+
+			if *addressToSet != addr.ipAddress {
+				log.Info().Msg("Ip address of A record did not match obtained address")
+				ddnsDNSARecordUpdateTimeGauge.WithLabelValues(*addressToSet, addr.aRecord).SetToCurrentTime()
+
+				if err := d.SetARecordAddress(*addressToSet, addr); err != nil {
+					return err
+				}
+			} else {
+				log.Info().Msgf("Ip address of A record matched obtained address, no update required")
+			}
+		}
+
+		return nil
+	}
+}
+
+func main() {
+	log.Debug().Msgf("Running ddns version %s", Version)
+
+	// Initialize Logging
+	logLevel := flag.String("logLevel", "info", "Log level, possible values: trace, debug, info, warn, error, fatal, panic")
+	configPath := flag.String("config", "./config.yml", "Relative or absolute path to the config file")
+	flag.Parse()
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	if l, err := zerolog.ParseLevel(*logLevel); err == nil {
+		zerolog.SetGlobalLevel(l)
+	} else {
+		log.Error().Msgf("Could not parse log level '%s', defaulting to 'info'", *logLevel)
+	}
+
+	// Initialize Config
+	c, err := NewConfig(*configPath)
+	if err != nil {
+		log.Fatal().Msgf("Error while loading the config file at %s: %s", *configPath, err)
+	}
+
+	// Initialize Metrics
+	if c.MetricsServerConfig.Enable {
+		ddnsVersionGauge.WithLabelValues("0.0.1", runtime.Version()).Set(1)
+		router := httprouter.New()
+		router.GET("/metrics", Metrics())
+
+		now := time.Now()
+		ddnsStartTimeGauge.WithLabelValues().Set(float64(now.Unix()))
+
+		ch := make(chan bool)
+		go func() {
+			listen := fmt.Sprintf("%s:%s", c.MetricsServerConfig.Host, c.MetricsServerConfig.Port)
+			log.Info().Msgf("Metrics endpoint listening on %s...", listen)
+			ch <- true
+			if err := http.ListenAndServe(listen, router); err != nil {
+				log.Fatal().Msgf("Could not listen on %s: %s", listen, err)
+			}
+		}()
+		<-ch
+	}
+
+	// Start Main Loop
+	i := IPAddressProviderFactory(c)
+	if i == nil {
+		log.Fatal().Msgf("No IPAddressProvider was configured and enabled")
+	}
+
+	d := DNSProviderFactory(c)
+	if d == nil {
+		log.Fatal().Msgf("No DNSProvider was configured and enabled")
+	}
+
+	// m, err := d.GetARecordAddresses()
+	// if err != nil {
+	// 	log.Fatal().Msg(err.Error())
+	// }
+
+	// for _, addr := range m {
+	// 	ddnsDNSARecordUpdateTimeGauge.WithLabelValues(addr.ipAddress, addr.aRecord).SetToCurrentTime()
+	// }
+
+	Retry(SyncRecords(i, d), c.WaitInterval, c.RetryInterval)
+}
+
+// Returns an instrance of IPAddressProvider based on the passed configuration
+func IPAddressProviderFactory(c *Config) IPAddressProvider {
+	if c.StaticIPAddressProviderConfig.Enable {
+		log.Debug().Msgf("Using StaticIPAddressProvider as IPAddressProvider")
+		return NewStaticIPAddressProvider(&c.StaticIPAddressProviderConfig)
+	} else if c.URLIPAddressProviderConfig.Enable {
+		log.Debug().Msgf("Using URLIPAddressProvider as IPAddressProvider")
+		return NewURLIPAddressProvider(&c.URLIPAddressProviderConfig)
+	}
+
+	return nil
+}
+
+// Returns an instrance of DNSProvider based on the passed configuration
+func DNSProviderFactory(c *Config) DNSProvider {
+	if c.CloudflareDNSProviderConfig.Enable {
+		log.Debug().Msgf("Using CloudflareDNSProvider as DNSProvider")
+		return NewCloudflareDNSProvider(&c.CloudflareDNSProviderConfig)
+	}
+
+	return nil
+}
